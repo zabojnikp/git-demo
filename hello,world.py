5f25107ad2b3a0a7d15bce877c894abd78fbf614
PetraZ <zabojnikova.petra@gmail.com>
2017-01-30 22:34:50 +0100

    finished for 1st round

--- hello,world.py
+++ hello,world.py
@@ -1,1 +1,25 @@
 print ("hello, world")
+
+class Kotatko:
+    def zamnoukej(self, zprava):
+        print("Kotatko: {}".format(zprava))
+
+kotatko = Kotatko()
+kotatko.zamnoukej("Mnaaau")
+
+class Zajicek:
+    def hopkej(self, zprava):
+        print("{}: {}".format(self.jmeno, zprava))
+
+zajicek = Zajicek()
+zajicek.jmeno = "Claudio"
+zajicek.hopkej("hop,hop")
+
+
+jinyzajicek = Zajicek()
+jinyzajicek.jmeno = "Osvald"
+jinyzajicek.hopkej('nebudu, hopkat')
+print("osvald je liny".capitalize())
+
+
+print("ahoj".capitalize())
